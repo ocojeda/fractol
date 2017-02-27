d17fadeb65bc31e7c6b760664067756d4cc1f4e8
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p4.42.fr>
2017-02-27 23:19:54 +0100

    gere des evenements souris

--- mandel.c
+++ mandel.c
@@ -6,7 +6,7 @@
 /*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 16:53:04 by ocojeda-          #+#    #+#             */
-/*   Updated: 2017/02/27 18:24:32 by tfaure           ###   ########.fr       */
+/*   Updated: 2017/02/27 23:19:22 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,21 @@ int             my_key_func(int keycode, t_screen *fst)
 	ft_putnbr(keycode);
 	if(keycode == 53)
 		exit (0);
-//	mlx_destroy_image(fst->mlx, fst->img);
-//	fst->img = mlx_new_image(fst->mlx, LEN, HEIGHT);
-//	mlx_put_image_to_window(fst->mlx, fst->win, fst->img, 0, 0);
+	mlx_destroy_image(fst->mlx, fst->img);
+	fst->img = mlx_new_image(fst->mlx, LEN, HEIGHT);
+	mlx_put_image_to_window(fst->mlx, fst->win, fst->img, 0, 0);
 	return (1);
 }
-
+int			mouse_hook(int button, int x, int y, t_screen *fst)
+{
+	ft_putnbr(button);
+	ft_putstr("-");
+	ft_putnbr(x);
+	ft_putstr("-");
+	ft_putnbr(y);
+	ft_putendl("-");
+	return (1);
+}
 void	ft_mandel(void)
 {
 	t_screen fst;
@@ -34,6 +43,7 @@ void	ft_mandel(void)
 	algo_mandel(&fst, 0);
 	fst.win = mlx_new_window(fst.mlx, LEN, HEIGHT, "mathilde aime trop la polla!");
 	mlx_put_image_to_window(fst.mlx, fst.win, fst.img, 0, 0);
+	mlx_hook(fst.win, 4, 3, mouse_hook, &fst);
 	mlx_hook(fst.win, 2, 3, my_key_func, &fst);
 	mlx_loop(fst.mlx);
 }
