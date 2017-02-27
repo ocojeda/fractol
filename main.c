a368a7712a6667b86301f0c3fbd71dd8fa3d21ee
Mathilde YERNAUX <myernaux@e2r13p2.42.fr>
2017-02-27 14:10:44 +0100

    c'est un debut bien nul

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myernaux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/27 13:46:15 by myernaux          #+#    #+#             */
+/*   Updated: 2017/02/27 14:08:34 by myernaux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.c"
+
+void	ft_fractol_available(void)
+{
+	LA IL ME FAUT AFFICHER LES FRACTALS DISPONIBLES
+}
+
+int		main(int ac, char **av)
+{
+	if (ac != 2)
+		ft_fractol_available(void);
+	if (ft_strcmp(av[1], "Julia") == 0)
+		ft_julia(void);
+	if (ft_strcmp(av[1], "Mandelbrot") == 0)
+		ft_mandel(void);
+	if (ft_strcmp(av[1], "third") == 0)
+		ft_third(void);
+	
+}
