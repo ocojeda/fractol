ed2f7db9152032ff241fce8ebcbdb80502a82fa7
Mathilde YERNAUX <myernaux@e2r13p4.42.fr>
2017-03-01 08:32:03 +0100

    BONNE FORMULE ASKIP (- -min + +min)

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: myernaux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 16:41:25 by myernaux          #+#    #+#             */
-/*   Updated: 2017/02/27 18:51:04 by tfaure           ###   ########.fr       */
+/*   Updated: 2017/03/01 08:31:55 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ float	ft_map(float x, float lenght, float min, float max)
 {
 	float	a;
 
-	return (a = (x / (lenght / (max - min))) - max);
+	return (a = (x / (lenght / (max - min))) + min);
 }
